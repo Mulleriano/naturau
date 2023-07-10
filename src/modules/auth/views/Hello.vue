d014df2293b8aaff5db314e083a909ccd804338b
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-10 11:16:53 -0300

    view de ola humano

---
+++ src/modules/auth/views/Hello.vue
@@ -0,0 +1,22 @@
+<script>
+// import { RouterLink, useRouter } from "vue-router";
+// const router = useRouter(); 
+// const click = () => {
+//      handleClick() 
+//          router.push("/pet-name");
+    
+// }
+export default  {
+    methods: {
+    handleClick() {
+       this.$router.push("/pet-name");
+     },
+},
+}
+</script>
+
+<template>
+<h1>Ola, humano.</h1>
+<p>Preparado para mudar a alimentação e invertir na saúde do seu melhor amigo?</p>
+<v-btn density="compact" icon="mdi-arrow-right-thin" size="x-large" @click="handleClick" variant="plain" location="bottom" position="fixed" class="mb-5"></v-btn>
+</template>
