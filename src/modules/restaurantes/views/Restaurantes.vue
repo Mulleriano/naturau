f38e967fcce0bd99daada3bed1a6b5f2ad5e40b2
Igor Muller <igormullersantos@hotmail.com>
2023-07-10 13:37:08 -0300

    altera nome da view e alimenta store na montagem

---
+++ src/modules/restaurantes/views/Restaurantes.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { onMounted } from "vue";
+import { restStore } from "../store";
+import RestList from "../components/RestList.vue";
+
+onMounted(() => {
+  restStore.getRestaurantes();
+});
+</script>
+<template>
+  <RestList />
+</template>
