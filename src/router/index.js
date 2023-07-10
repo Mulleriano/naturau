86741fe849c93e38b707c18a29fcda98082a2746
Igor Muller <igormullersantos@hotmail.com>
2023-07-10 11:22:30 -0300

    Criação da rota do aplicativo

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,20 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/naturau",
+    component: () => import("@/layouts/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "RestList",
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ "@/modules/restaurantes/views/RestList.vue"
+          ),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
