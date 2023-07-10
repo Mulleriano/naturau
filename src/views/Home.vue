f8561fc4e74c83129c07053c0060c3fa9c4e5310
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-10 13:51:34 -0300

    tirando os comentarios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,16 +16,6 @@ export default  {
 
 </template>
 
-<!-- <template>
-  <main class="w-100 h-100">
-    <img src="/public/bg.jpg" class="w-auto h-auto overflow">
-    <v-card location="bottom" position="fixed" class="mb-10 elevation-0" color="transparent" > 
-    <v-btn density="compact" icon="mdi-arrow-right-thin" size="x-large" @click="handleClick" variant="plain"></v-btn>
-    <v-btn @click="handleClick"  variant="plain"> Next </v-btn> 
-    </v-card>
-  </main>
-</template> -->
-
 <style>
 
 body {
