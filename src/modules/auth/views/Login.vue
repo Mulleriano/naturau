11de4ce1a031da93d309be6b5a8457b348fb6392
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-10 14:37:36 -0300

    v-main + google

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,12 +1,12 @@
 <script>
 import { ref } from "vue";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, } from "firebase/auth";
 import { RouterLink, useRouter } from "vue-router";
 
 const email = ref("");
 const password = ref("");
 const errMsg = ref(); //msg para erro
-const router = useRouter(); 
+const router = useRouter();
 
 //metodos
 const login = () => {
@@ -33,24 +33,56 @@ const login = () => {
     });
 };
 
-const signInWithGoogle = () => {};
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      router.push("/");
+    })
+    .catch((error) => {
+      alert("Algo deu errado");
+    });
+};
 </script>
 
 <template>
-  <body class="w-100 h-100 d-flex flex-column align-center justify-center">
+  <v-main class="w-100 h-100 d-flex flex-column align-center justify-center">
     <h1>Login</h1>
-    <v-text-field v-model="email" label="Email" clearable hide-details="auto" class="w-75">
+    <v-text-field
+      v-model="email"
+      label="Email"
+      clearable
+      hide-details="auto"
+      class="w-75"
+    >
     </v-text-field>
-    <v-text-field v-model="password" type="password" label="Senha" clearable hide-details="auto" class="w-75">
+    <v-text-field
+      v-model="password"
+      type="password"
+      label="Senha"
+      clearable
+      hide-details="auto"
+      class="w-75"
+    >
     </v-text-field>
     <p v-if="errMsg">{{ errMsg }}</p>
-    <v-btn @click="login" variant="outlined" size="small" class="mt-2">Logar</v-btn>
-    <v-btn @click="signInWithGoogle" variant="outlined" size="small" class="mt-2"
+    <v-btn @click="login" variant="outlined" size="small" class="mt-2"
+      >Logar</v-btn
+    >
+    <v-btn
+      @click="signInWithGoogle"
+      variant="outlined"
+      size="small"
+      class="mt-2"
       >Entre com o Google</v-btn
     >
-    <p>Não tem cadastro? <router-link class="text-decoration-none"
-          to="/register">Registre-se</router-link></p>
-  </body>
+    <p>
+      Não tem cadastro?
+      <router-link class="text-decoration-none" to="/register"
+        >Registre-se</router-link
+      >
+    </p>
+  </v-main>
 </template>
 
 <style>
