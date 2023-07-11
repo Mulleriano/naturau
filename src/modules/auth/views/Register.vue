885b3b9502430cba7b7329fd5fe61258556f74a8
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-11 15:12:19 -0300

    importação do googleAuth e adição da funcionalidade de entrar com o google; Criação da regra para confirmar a senha; Criação da constante de validar o formulario para desabilitar o botão de cadastrar.

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -1,11 +1,18 @@
 <script setup>
 import { ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { useRouter } from "vue-router";
 
 const email = ref("");
 const password = ref("");
-const router = useRouter(); 
+const confpass = ref("");
+const isFormValid = ref(true);
+const router = useRouter();
 
 //metodos
 const register = () => {
@@ -14,7 +21,7 @@ const register = () => {
     //retorna uma promese
     .then((data) => {
       alert("Registrado com sucesso");
-      router.push("/hello")
+      router.push("/hello");
     })
     .catch((error) => {
       console.log(error.code);
@@ -22,12 +29,30 @@ const register = () => {
     });
 };
 
-const signInWithGoogle = () => {};
+const passwordconf = (value) => {
+  if (value !== password.value) return "Senha incorreta";
+  return true;
+};
+
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      router.push("/");
+    })
+    .catch((error) => {
+      alert("Algo deu errado");
+    });
+};
 </script>
 
 <template>
-
-    <h1>Cadastro</h1>
+  <!-- Adicionar na branch css -> 
+        - adicionar ou trocar o v-card por um v-form e adicinar = @submit.prevent v-model="isFormValid"
+        - add :rules="[passwordconf]" no confirmaçao de senha
+        - :disabled="isFormValid" no botao de cadastrar  -->
+        
+  <h1>Cadastro</h1>
     <v-text-field v-model="email" label="Email" clearable hide-details="auto" class="w-75">
     </v-text-field>
     <v-text-field v-model="password" type="password" label="Senha" clearable hide-details="auto" class="w-75">
@@ -36,5 +61,4 @@ const signInWithGoogle = () => {};
     <v-btn @click="signInWithGoogle" variant="outlined" size="small" class="mt-2"
       >Entre com o Google</v-btn
     >
-
 </template>
