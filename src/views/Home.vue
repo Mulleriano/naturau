9b3a49f0432b7bb71331bbc93881b2de0605133b
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-11 13:09:00 -0300

    tamanho da pg do home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
 
 <template>
   <v-app>
-   <v-container class="bg">
+   <v-container class="w-100 h-100 bg">
     <v-btn density="compact" icon="mdi-arrow-right-thin" size="x-large" @click="this.$router.push('/login')" variant="plain" location="bottom" position="fixed" class="mb-5"></v-btn>
    </v-container> 
   </v-app>
