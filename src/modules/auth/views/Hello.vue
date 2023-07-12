572cfe3d93fd272c1db0a8bc1977d36ad9ac0823
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-12 09:19:59 -0300

    estilização

--- src/modules/auth/views/Hello.vue
+++ src/modules/auth/views/Hello.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  
-    <h1>Ola, humano.</h1>
-<p>Preparado para mudar a alimentação e invertir na saúde do seu melhor amigo?</p>
-<v-btn density="compact" icon="mdi-arrow-right-thin" size="x-large" @click="this.$router.push('/pet-name')" variant="plain" location="bottom" position="fixed" class="mb-5"></v-btn>
-  
+  <v-container class="pl-8 h-25 w-100 d-flex flex-column justify-center"  >
+        <h3 class="mb-2">Olá, humano.</h3>
+<p class="text-start w-75">Preparado para mudar a <br> alimentação e invertir na saúde <br> do seu melhor amigo?</p>
+   </v-container>
+    <v-btn density="compact" icon="mdi-arrow-right-thin" size="x-large" @click="this.$router.push('/pet-name')" variant="plain" location="bottom" position="fixed" class="mb-5"></v-btn>
 </template>
