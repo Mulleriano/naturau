d3341056b1ebef698094b56ef271ab0e807f1c49
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-12 14:57:12 -0300

    apenas css

--- src/modules/auth/views/PetDetail.vue
+++ src/modules/auth/views/PetDetail.vue
@@ -1,34 +1,92 @@
-<script>
+<script setup>
 //
 </script>
 
 <template>
- 
-    <h2>Nosso papo agora é com X{{ petname }}</h2>
-
-    <p>Qual tipo de pet você é?</p>
-    <div>
-      <v-btn size="small" variant="plain" class="mb-5">Gato</v-btn>
-      <v-btn size="small" variant="plain" class="mb-5">Cachorro</v-btn>
-    </div>
-    <p>Qual é seu sexo?</p>
-    <div>
-      <v-btn size="small" variant="plain" class="mb-5">Fêmea</v-btn>
-      <v-btn size="small" variant="plain" class="mb-5">Macho</v-btn>
-    </div>
-    <p>Tem algo que você não pode comer?</p>
-    <div>
-      <v-btn size="small" variant="plain" class="mb-5">Não</v-btn>
-      <v-btn size="small" variant="plain" class="mb-5">Sim</v-btn>
-    </div>
-    <v-btn
-      size="x-large"
-      @click="this.$router.push('/')"
-      variant="plain"
-      location="bottom"
-      position="fixed"
-      class="mb-5"
-      >Próximo</v-btn
-    >
-  
+  <v-card
+    position="relative"
+    location="center"
+    elevation="0"
+    class="h-75 w-100 pa-8 d-flex flex-column justify-center"
+  >
+    <h2 class="mb-4 text-teal-darken-3">
+      Nosso papo agora <br />
+      é com X{{ petname }}
+    </h2>
+    <v-item-group mandatory>
+      <p class="text-body-1">Qual tipo de pet você é?</p>
+      <div class="d-flex justify-space-around">
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5 pl-10 pr-10"
+            >Gato</v-btn
+          >
+        </v-item>
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5"
+            >Cachorro</v-btn
+          >
+        </v-item>
+      </div>
+      <p class="text-body-1">Qual é seu sexo?</p>
+      <div class="d-flex justify-space-around mt-2">
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5 pl-8 pr-8"
+            >Fêmea</v-btn
+          >
+        </v-item>
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5 pl-8 pr-8"
+            >Macho</v-btn
+          >
+        </v-item>
+      </div>
+      <p class="text-body-1">Tem algo que você não pode comer?</p>
+      <div class="d-flex justify-space-around mt-2">
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5 pl-11 pr-11"
+            >Não</v-btn
+          >
+        </v-item>
+        <v-item v-slot="{ isSelected, toggle }">
+          <v-btn
+            @click="toggle"
+            :color="isSelected ? '#8dd8c1' : 'white'"
+            rounded="xl"
+            class="ma-5 pl-12 pr-12"
+            >Sim</v-btn
+          >
+        </v-item>
+      </div>
+    </v-item-group>
+  </v-card>
+  <v-btn
+    size="large"
+    @click="this.$router.push('/naturau')"
+    rounded="xl"
+    color="#8ad7c0"
+    location="bottom"
+    position="fixed"
+    class="mb-16 w-50"
+    >Próximo</v-btn
+  >
 </template>
