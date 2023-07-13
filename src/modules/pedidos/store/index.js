cba1015db55164844334221613c7e3653ec83cd8
Igor Muller <igormullersantos@hotmail.com>
2023-07-13 12:05:39 -0300

    Criação da função para filtrar o pedido selecionado

--- src/modules/pedidos/store/index.js
+++ src/modules/pedidos/store/index.js
@@ -3,8 +3,20 @@ import { listaPedidos } from "../api";
 
 export const pedidosStore = reactive({
   pedidos: [],
+  pedido: {},
+  loadingDetalhes: true,
   async pegarPedidos() {
     const res = await listaPedidos();
     this.pedidos = res;
+  },
+  async filtrarPedidos(id) {
+    await this.pegarPedidos();
+    this.pedidos.concluidos.map((a) => {
+      if (a.id == id) this.pedido = a;
+    });
+    this.pedidos.emAndamento.map((a) => {
+      if (a.id == id) this.pedido = a;
+    });
+    this.loadingDetalhes = false;
   },
 });
