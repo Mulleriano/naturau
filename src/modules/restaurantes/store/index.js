a9d8fd769fd574963d8737c09dba3ade615e48c8
Igor Muller <igormullersantos@hotmail.com>
2023-07-13 17:29:40 -0300

    Criação das funções que armazenam informações de comida e restaurante

--- src/modules/restaurantes/store/index.js
+++ src/modules/restaurantes/store/index.js
@@ -1,18 +1,30 @@
 import { reactive } from "vue";
-import { listaRestaurantes, listaComidas } from "../api";
+import { listaRestaurantes, restDados, listaComidas } from "../api";
 
 export const restStore = reactive({
   restaurantes: [],
+  restaurante: {},
   async pegarRestaurantes() {
     const res = await listaRestaurantes();
     this.restaurantes = res;
+  },
+  async pegarDadosRest(restId) {
+    const res = await restDados(restId);
+    this.restaurante = res;
   },
 });
 
 export const comidasStore = reactive({
   comidas: [],
+  comida: {},
   async pegarComidas(restId) {
     const res = await listaComidas(restId);
     this.comidas = res;
+  },
+  async filtrarComida(restId, comidaId) {
+    await this.pegarComidas(restId);
+    this.comidas.map((comida) => {
+      if (comidaId == comida.id) this.comida = comida;
+    });
   },
 });
