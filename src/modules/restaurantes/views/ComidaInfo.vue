d66d6019e0e4592e1b9d79cc05f3cbd4c291eca2
Igor Muller <igormullersantos@hotmail.com>
2023-07-13 17:30:21 -0300

    Criação da view de detalhes da comida

---
+++ src/modules/restaurantes/views/ComidaInfo.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { onMounted } from "vue";
+import { comidasStore } from "../store";
+import { useRoute } from "vue-router";
+import { restStore } from "../store";
+
+const route = useRoute();
+const restId = route.params.restId;
+const comidaId = route.params.comidaId;
+
+onMounted(async () => {
+  await restStore.pegarDadosRest(restId);
+  await comidasStore.filtrarComida(restId, comidaId);
+});
+</script>
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon @click="this.$router.go(-1)"
+      ><v-icon>mdi-arrow-left</v-icon></v-app-bar-nav-icon
+    >
+    <v-app-bar-title> {{ comidasStore.comida.titulo }} </v-app-bar-title>
+  </v-app-bar>
+
+  <v-card position="fixed" location="top" width="100%">
+    <v-img cover :src="comidasStore.comida.img"></v-img>
+  </v-card>
+
+  <div>
+    <v-card
+      height="auto"
+      width="100%"
+      color="#fafafa"
+      position="fixed"
+      style="top: 30%; right: 0"
+      class="pb-4 rounded-xl elevation-10 d-flex justify-space-between align-center pa-3"
+    >
+      <div>
+        <v-card-title class="text-orange">
+          Por {{ restStore.restaurante.nome }}
+        </v-card-title>
+        <v-card-subtitle>
+          <v-icon>mdi-map-marker</v-icon>
+          Distância {{ restStore.restaurante.distancia }}
+        </v-card-subtitle>
+      </div>
+      <div class="text-end pt-2">
+        <v-rating
+          size
+          v-model="restStore.restaurante.avaliacoes"
+          half-increments
+          readonly
+        >
+        </v-rating>
+        <p color="#fe8b05">R$ {{ comidasStore.comida.preco?.toFixed(2) }}</p>
+      </div>
+    </v-card>
+
+    <v-sheet
+      width="80%"
+      height="100px"
+      position="fixed"
+      location="center"
+      color="transparent"
+    >
+      <v-btn
+        @click="adicionarPedido(restId, comida)"
+        color="#fe8b05"
+        block
+        rounded
+        class="pa-7 elevation-10 text-white"
+      >
+        Pedir R${{ comidasStore.comida.preco?.toFixed(2) }}
+      </v-btn>
+    </v-sheet>
+  </div>
+</template>
