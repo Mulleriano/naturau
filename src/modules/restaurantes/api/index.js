211848086eef5f3bc14766fafe2d19bbe11170b7
Igor Muller <igormullersantos@hotmail.com>
2023-07-13 17:29:00 -0300

    Função que recebe dados do restaurante baseado no id

--- src/modules/restaurantes/api/index.js
+++ src/modules/restaurantes/api/index.js
@@ -1,15 +1,28 @@
 import { db } from "@/firebase.config";
-import { collection, collectionGroup, getDocs } from "firebase/firestore";
+import {
+  collection,
+  collectionGroup,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
 export async function listaRestaurantes() {
-  const querySnapshot = await getDocs(collection(db, "restaurantes"));
-
   let itensLocal = [];
-
+  const querySnapshot = await getDocs(collection(db, "restaurantes"));
   querySnapshot.forEach((doc) => {
     itensLocal.push({ ...doc.data(), id: doc.id });
   });
   return itensLocal;
+}
+
+export async function restDados(restId) {
+  let restData = {};
+  const restaurantes = await getDoc(doc(db, "restaurantes", restId));
+
+  restData = restaurantes.data();
+
+  return restData;
 }
 
 export async function listaComidas(restId) {
