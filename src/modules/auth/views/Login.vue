ab133fe5ca2075cb88cfb2febbb76c5c6caa055e
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-13 10:29:54 -0300

    Adição do icone para visualizar a senha em texto e desabilitando botão de login.

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -10,15 +10,15 @@ import { RouterLink, useRouter } from "vue-router";
 
 const email = ref("");
 const password = ref("");
-const isFormValid = ref(true);
 const errMsg = ref();
 const router = useRouter();
+const show1 = ref(false);
 
 //metodos
 const login = () => {
   //precisa de .value por conta do ref()
   signInWithEmailAndPassword(getAuth(), email.value, password.value)
-    //retorna uma promese
+    //retorna uma promise
     .then((data) => {
       alert("Login realizado com sucesso!");
       router.push("/naturau");
@@ -33,7 +33,7 @@ const login = () => {
           errMsg.value = "Senha incorreta";
           break;
         default:
-          errMsg.value = "Email ou senha incorretos";
+          errMsg.value = "Email ou senha incorreto";
           break;
       }
     });
@@ -52,51 +52,63 @@ const signInWithGoogle = () => {
 </script>
 
 <template>
-<v-form @submit.prevent v-model="isFormValid" class="w-100 h-100 d-flex align-center justify-center">
-    <v-card title="Login" elevation="0" class="w-75  d-flex flex-column align-center">
-          <v-text-field
+  <v-form class="w-100 h-100 d-flex align-center justify-center">
+    <v-card
+      title="Login"
+      elevation="0"
+      class="w-75 d-flex flex-column align-center"
+    >
+      <v-text-field
         variant="outlined"
-      v-model="email"
-      label="Email"
-      clearable
-      hide-details="auto"
-      class="w-100 ma-4"
-    >
-    </v-text-field>
-    <v-text-field
-    variant="outlined"
-      v-model="password"
-      type="password"
-      label="Senha"
-      clearable
-      hide-details="auto"
-      class="w-100 ma-4"
-    >
-    </v-text-field>
-    <p v-if="errMsg">{{ errMsg }}</p>
-    <v-card-actions class="d-flex flex-column">
-        <v-btn @click="login" rounded="xl"
-      color="#2a6141" variant="tonal" class="mt-2" block
-      >Entrar</v-btn
-    >
-        <v-btn
-        :disabled="isFormValid"
-      @click="signInWithGoogle"
-      variant="outlined"
-      rounded="xl"
-      class="mt-4"
-      prepend-icon="mdi-google"
-      >Entre com o Google</v-btn
-    >
-    <p class="mt-4">
-      Não tem cadastro?
-      <router-link class="text-decoration-none" to="/register"
-        >Registre-se</router-link
+        v-model="email"
+        label="Email"
+        clearable
+        hide-details="auto"
+        class="w-100 ma-4"
       >
-    </p>
-    </v-card-actions>
-    </v-card>    
-    </v-form>
+      </v-text-field>
+      <v-text-field
+        variant="outlined"
+        :append-inner-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show1 ? 'text' : 'password'"
+        @click:append-inner="show1 = !show1"
+        v-model="password"
+        type="password"
+        label="Senha"
+        clearable
+        hide-details="auto"
+        class="w-100 ma-4"
+      >
+      </v-text-field>
+      <p v-if="errMsg">{{ errMsg }}</p>
+      <v-card-actions class="d-flex flex-column">
+        <v-btn
+          @click="login"
+          :disabled="!email || !password"
+          rounded="xl"
+          color="#2a6141"
+          variant="tonal"
+          class="mt-2"
+          block
+          >Entrar</v-btn
+        >
+        <v-btn
+          @click="signInWithGoogle"
+          variant="outlined"
+          rounded="xl"
+          class="mt-4"
+          prepend-icon="mdi-google"
+          >Entre com o Google</v-btn
+        >
+        <p class="mt-4">
+          Não tem cadastro?
+          <router-link class="text-decoration-none" to="/register"
+            >Registre-se</router-link
+          >
+        </p>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 
 <style>
