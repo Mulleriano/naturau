e379893510282992c9ee38358ed2c2a6cf1f1d0c
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-14 15:19:14 -0300

    utiliza a função fim do store para adicionar novas informações no documento do usuario; Criação de uma função para tornar o input de restrição em um array.

--- src/modules/auth/views/PetDetail.vue
+++ src/modules/auth/views/PetDetail.vue
@@ -1,5 +1,46 @@
 <script setup>
-//
+import {petStore} from "../store"
+import { reactive } from "vue";
+import { ref } from "vue";
+
+const restricao = ref("");
+
+const payload = reactive({ 
+  tipo: "",
+  sexo: "",
+  sn: "",
+  restricao: [],
+})
+
+function arrayRestricao(restricao){
+  let semVirgula = restricao.replaceAll(",", "")
+  payload.restricao = semVirgula.split(" ")
+  console.log(semVirgula)
+}
+
+function handleDetail(toggle, escolha){
+  toggle();
+  switch(escolha){
+    case "gato":
+    payload.tipo = escolha;
+    break;
+    case "cachorro":
+    payload.tipo = escolha;
+    break;
+    case "femea":
+    payload.sexo = escolha;
+    break;
+    case "macho":
+    payload.sexo = escolha;
+    break;
+    case "sim":
+    payload.sn = escolha;
+    break;
+    case "nao":
+    payload.sn = escolha;
+    break;
+  }
+}
 </script>
 
 <template>
@@ -18,7 +59,7 @@
       <div class="d-flex justify-space-around">
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'gato')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5 pl-10 pr-10"
@@ -27,7 +68,7 @@
         </v-item>
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'cachorro')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5"
@@ -35,11 +76,13 @@
           >
         </v-item>
       </div>
+    </v-item-group>
+    <v-item-group mandatory>
       <p class="text-body-1">Qual é seu sexo?</p>
       <div class="d-flex justify-space-around mt-2">
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'femea')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5 pl-8 pr-8"
@@ -48,7 +91,7 @@
         </v-item>
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'macho')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5 pl-8 pr-8"
@@ -56,11 +99,13 @@
           >
         </v-item>
       </div>
+    </v-item-group>
+    <v-item-group mandatory>
       <p class="text-body-1">Tem algo que você não pode comer?</p>
       <div class="d-flex justify-space-around mt-2">
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'nao')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5 pl-11 pr-11"
@@ -69,7 +114,7 @@
         </v-item>
         <v-item v-slot="{ isSelected, toggle }">
           <v-btn
-            @click="toggle"
+            @click="handleDetail(toggle, 'sim')"
             :color="isSelected ? '#8dd8c1' : 'white'"
             rounded="xl"
             class="ma-5 pl-12 pr-12"
@@ -78,10 +123,22 @@
         </v-item>
       </div>
     </v-item-group>
+    <v-textarea
+      rows="1"
+      variant="underlined"
+      color="#2a6141"
+      v-model="restricao"
+      label="Restrições"
+      placeholder="Nos conte o que é"
+      clearable
+      hide-details="auto"
+      class="w-100 mt-4 text-teal-darken-2"
+    >
+    </v-textarea>
   </v-card>
   <v-btn
     size="large"
-    @click="this.$router.push('/naturau')"
+    @click="(petStore.fim(payload)), (this.$router.push('/naturau')), (arrayRestricao(restricao))"
     rounded="xl"
     color="#8ad7c0"
     location="bottom"
