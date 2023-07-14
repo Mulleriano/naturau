8189f0686ba3a6a787a1278098e769639e153019
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-14 15:14:29 -0300

    cria as funcoes utilizadas na view pet-name e pet-detail, as quais capturam o id do usario logado para criar um novo documento e adicionar informações neste documento.

---
+++ src/modules/auth/store/index.js
@@ -0,0 +1,26 @@
+import { db } from "@/firebase.config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore"; 
+import { reactive } from "vue";
+
+
+ export const petStore = reactive({
+ proximo: (payload) => {
+    onAuthStateChanged(getAuth(), (user) => {
+       const uid = user.uid
+     console.log(uid)
+     const usuario = setDoc(doc(db, "usuarios", uid),{
+      payload
+     }).catch((error)  => {
+         console.log(error.code);
+         alert(error.message);
+     })
+    })},
+  fim: (payload) => {
+    onAuthStateChanged(getAuth(), (user) => {
+      const uid = user.uid
+    console.log(uid)
+    const usuarios = doc(db, "usuarios", uid);
+    setDoc(usuarios,  { payload} , { merge: true })})},
+  
+    });
