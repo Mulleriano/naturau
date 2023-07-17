93489a10e983b9d94e9b3ab72c23f88a75865a28
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-17 15:12:11 -0300

    view de perfil que nao esta pronta

---
+++ src/modules/auth/views/Perfil.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRoute } from "vue-router";
+import { petDados, petStore } from "../store";
+
+const isLoggedIn = ref(false);
+const route = useRoute();
+
+
+onMounted(async () => {
+  await petStore.pegarDadosPet();
+});
+
+
+/* onMounted(()=> { */
+  auth = getAuth();
+onAuthStateChanged(auth, (user)=> {
+  if(user){
+    isLoggedIn.value = true;
+  } else {
+    isLoggedIn.value = false;
+  }
+});
+
+
+
+const handleSignOut = () => {
+  signOut(auth).then(()=>{
+    router.push("/login");
+  })
+};
+</script>
+
+<template>
+  
+  <v-app-bar>
+    <v-app-bar-nav-icon @click="this.$router.go(-1)"
+      ><v-icon>mdi-arrow-left</v-icon></v-app-bar-nav-icon
+    >
+    <v-app-bar-title> Perfil do pet</v-app-bar-title>
+  </v-app-bar>
+  <v-container class="w-100 mt-16 bg-green d-flex flex-column align-center">
+    <v-img
+    cover
+      width="100%"
+     
+      class="mt-5 bg-red"
+    >
+    </v-img>
+    <v-card elevation="0" width="85%" position="absolute" location="bottom"  class="d-flex mb-2" rounded="lg">
+  <v-card elevation="0"  width="80%">
+  <h3 class="ma-2">Ivy</h3>
+  <p class="text-caption ma-2"> <v-icon icon="mdi-map-marker-outline pb-1"></v-icon> Rua tals lala e la, 123 </p>
+  </v-card>
+  <v-card elevation="0" width="20%">
+    <v-icon icon="mdi-pencil ma-2"></v-icon>
+    <p class="text-caption ma-2">Fêmea</p>
+  </v-card>
+  </v-card>
+    
+  </v-container>
+  
+<v-btn @click="handleSignOut" > Sign Out</v-btn>
+</template>
