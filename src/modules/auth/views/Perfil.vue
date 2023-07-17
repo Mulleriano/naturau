e5119fe37d1389b19e0ca3b3c79adcb080d7c9ac
Igor Muller <igormullersantos@hotmail.com>
2023-07-17 15:55:25 -0300

    Removendo código não utilizado + code Prettier

--- src/modules/auth/views/Perfil.vue
+++ src/modules/auth/views/Perfil.vue
@@ -1,39 +1,25 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { useRoute } from "vue-router";
-import { petDados, petStore } from "../store";
+import { onMounted } from "vue";
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+import { petStore } from "../store";
 
-const isLoggedIn = ref(false);
-const route = useRoute();
-
+const router = useRouter();
 
 onMounted(async () => {
-  await petStore.pegarDadosPet();
+  await petStore.pegarUser();
 });
 
+const auth = getAuth();
 
-/* onMounted(()=> { */
-  auth = getAuth();
-onAuthStateChanged(auth, (user)=> {
-  if(user){
-    isLoggedIn.value = true;
-  } else {
-    isLoggedIn.value = false;
-  }
-});
-
-
-
-const handleSignOut = () => {
-  signOut(auth).then(()=>{
+function handleSignOut() {
+  signOut(auth).then(() => {
     router.push("/login");
-  })
-};
+  });
+}
 </script>
 
 <template>
-  
   <v-app-bar>
     <v-app-bar-nav-icon @click="this.$router.go(-1)"
       ><v-icon>mdi-arrow-left</v-icon></v-app-bar-nav-icon
@@ -41,25 +27,28 @@ const handleSignOut = () => {
     <v-app-bar-title> Perfil do pet</v-app-bar-title>
   </v-app-bar>
   <v-container class="w-100 mt-16 bg-green d-flex flex-column align-center">
-    <v-img
-    cover
-      width="100%"
-     
-      class="mt-5 bg-red"
+    <v-img cover width="100%" class="mt-5 bg-red"> </v-img>
+    <v-card
+      elevation="0"
+      width="85%"
+      position="absolute"
+      location="bottom"
+      class="d-flex mb-2"
+      rounded="lg"
     >
-    </v-img>
-    <v-card elevation="0" width="85%" position="absolute" location="bottom"  class="d-flex mb-2" rounded="lg">
-  <v-card elevation="0"  width="80%">
-  <h3 class="ma-2">Ivy</h3>
-  <p class="text-caption ma-2"> <v-icon icon="mdi-map-marker-outline pb-1"></v-icon> Rua tals lala e la, 123 </p>
-  </v-card>
-  <v-card elevation="0" width="20%">
-    <v-icon icon="mdi-pencil ma-2"></v-icon>
-    <p class="text-caption ma-2">Fêmea</p>
-  </v-card>
-  </v-card>
-    
+      <v-card elevation="0" width="80%">
+        <h3 class="ma-2">Ivy</h3>
+        <p class="text-caption ma-2">
+          <v-icon icon="mdi-map-marker-outline pb-1"></v-icon> Rua tals lala e
+          la, 123
+        </p>
+      </v-card>
+      <v-card elevation="0" width="20%">
+        <v-icon icon="mdi-pencil ma-2"></v-icon>
+        <p class="text-caption ma-2">Fêmea</p>
+      </v-card>
+    </v-card>
   </v-container>
-  
-<v-btn @click="handleSignOut" > Sign Out</v-btn>
+
+  <v-btn @click="handleSignOut"> Sign Out</v-btn>
 </template>
