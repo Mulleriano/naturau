30e8bde37d40d8104435a26efa2343986882d201
Igor Muller <igormullersantos@hotmail.com>
2023-07-17 17:19:27 -0300

    Estilizando a view de perfil

--- src/modules/auth/views/Perfil.vue
+++ src/modules/auth/views/Perfil.vue
@@ -26,29 +26,50 @@ function handleSignOut() {
     >
     <v-app-bar-title> Perfil do pet</v-app-bar-title>
   </v-app-bar>
-  <v-container class="w-100 mt-16 bg-green d-flex flex-column align-center">
-    <v-img cover width="100%" class="mt-5 bg-red"> </v-img>
-    <v-card
-      elevation="0"
-      width="85%"
-      position="absolute"
-      location="bottom"
-      class="d-flex mb-2"
-      rounded="lg"
-    >
-      <v-card elevation="0" width="80%">
-        <h3 class="ma-2">Ivy</h3>
-        <p class="text-caption ma-2">
-          <v-icon icon="mdi-map-marker-outline pb-1"></v-icon> Rua tals lala e
-          la, 123
-        </p>
-      </v-card>
-      <v-card elevation="0" width="20%">
-        <v-icon icon="mdi-pencil ma-2"></v-icon>
-        <p class="text-caption ma-2">Fêmea</p>
-      </v-card>
-    </v-card>
-  </v-container>
 
-  <v-btn @click="handleSignOut"> Sign Out</v-btn>
+  <v-img width="100%" height="50%"></v-img>
+
+  <v-card
+    color="#053026"
+    width="90%"
+    elevation="10"
+    rounded="xl"
+    class="d-flex justify-space-between align-center py-4 mx-auto"
+  >
+    <div class="w-50 ml-5">
+      <v-card-title class="text-white text-h4">{{
+        petStore.petDados.petName
+      }}</v-card-title>
+      <v-card-subtitle class="text-white py-4">
+        <v-icon color="white" icon="mdi-map-marker-outline pb-1"></v-icon>
+        {{ petStore.petDados?.address }}
+      </v-card-subtitle>
+    </div>
+
+    <div class="w-50 text-right mr-5">
+      <v-icon color="#8dd8c1" icon="mdi-pencil ma-2"></v-icon>
+      <p class="text-white pt-4">Fêmea</p>
+    </div>
+  </v-card>
+
+  <v-card elevation="10" class="mt-4">
+    <v-card-title>Restrições</v-card-title>
+    <v-card-text>
+      <p class="py-2" v-for="restricao in petStore.petDados.restricao">
+        {{ restricao }}
+      </p>
+    </v-card-text>
+  </v-card>
+
+  <v-sheet class="mx-auto mt-5" width="80%" height="100px" color="transparent">
+    <v-btn
+      @click="handleSignOut"
+      color="#fe8b05"
+      block
+      rounded
+      class="pa-7 elevation-10 text-white"
+    >
+      Log Out
+    </v-btn>
+  </v-sheet>
 </template>
