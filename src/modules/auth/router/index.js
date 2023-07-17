2846459feeec5c1c28464993351bacc3a613685b
Igor Muller <igormullersantos@hotmail.com>
2023-07-17 17:19:00 -0300

    Atualizando a rota de perfil para o componenete

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,43 +1,49 @@
 export const routes = [
   {
-      path: "/",
-      component: () => import ("../layouts/Access.vue"),
-      children: [
-        {
-          path: "login",
-          name: "Login",
-          component: () => import ("../views/Login.vue"),
-        },
-        {
-          path: "register",
-          name: "Register",
-          component: () => import ("../views/Register.vue"),
-        },
-        {
-          path: "hello",
-          name: "Hello",
-          component: () => import ("../views/Hello.vue"),
-        },
-        {
-          path: "pet-name",
-          name: "PetName",
-          component: () => import ("../views/PetName.vue"),
-        },
-        {
-          path: "pet-detail",
-          name: "PetDetail",
-          component: () => import ("../views/PetDetail.vue"),
-        },
-        {
-          path: "perfil",
-          name: "Perfil",
-          component: () => import ("../views/Perfil.vue"),
-        },
-        {
-          path: "pet-foto",
-          name: "PetFoto",
-          component: () => import ("../views/PetFoto.vue"),
-        },
-      ]
-    }
+    path: "/",
+    component: () => import("../layouts/Access.vue"),
+    children: [
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("../views/Login.vue"),
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: () => import("../views/Register.vue"),
+      },
+      {
+        path: "hello",
+        name: "Hello",
+        component: () => import("../views/Hello.vue"),
+      },
+      {
+        path: "pet-name",
+        name: "PetName",
+        component: () => import("../views/PetName.vue"),
+      },
+      {
+        path: "pet-detail",
+        name: "PetDetail",
+        component: () => import("../views/PetDetail.vue"),
+      },
+      {
+        path: "pet-foto",
+        name: "PetFoto",
+        component: () => import("../views/PetFoto.vue"),
+      },
+    ],
+  },
+  {
+    path: "/perfil",
+    component: () => import("@/layouts/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "Perfil",
+        component: () => import("../views/Perfil.vue"),
+      },
+    ],
+  },
 ];
