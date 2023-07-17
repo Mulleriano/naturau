17b8d627473f4ca08e095149b77c469f1e8b0d0a
Igor Muller <igormullersantos@hotmail.com>
2023-07-17 15:54:53 -0300

    Armazenando dados do pet na store

--- src/modules/auth/store/index.js
+++ src/modules/auth/store/index.js
@@ -1,17 +1,17 @@
 import { db } from "@/firebase.config";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { reactive } from "vue";
-
-
+import { dadosPet } from "../api";
 
 export const petStore = reactive({
   user: {},
   petDados: {},
   async pegarUser() {
-    await onAuthStateChanged(getAuth(), (user) => {
+    onAuthStateChanged(getAuth(), async (user) => {
       this.user = user;
-      return user;
+      const res = await dadosPet(user.uid);
+      this.petDados = res;
     });
   },
   proximo(payload) {
@@ -24,12 +24,18 @@ export const petStore = reactive({
   },
   fim(payload) {
     const usuarios = doc(db, "usuarios", this.user.uid);
-    setDoc(usuarios, { tipo: payload.tipo, sexo: payload.sexo, sn: payload.sn, restricao: payload.restricao  }, { merge: true });
+    setDoc(
+      usuarios,
+      {
+        tipo: payload.tipo,
+        sexo: payload.sexo,
+        sn: payload.sn,
+        restricao: payload.restricao,
+      },
+      { merge: true }
+    );
   },
-  pegarDadosPet(user) {
-    const usuarios =  doc(db, "usuarios", this.user.uid);
-    const pet = getDoc(usuarios);
-    const petDados = pet.data()
-    return petDados;
+  pegarPet(uid) {
+    console.log(uid);
   },
 });
