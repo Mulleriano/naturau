39bea7be25ac85e68f6ae9d6acf4e2f94cb13ec5
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-07-17 20:55:03 -0300

    adicionando os dados do pet, um dialog para trocar de foto e criando uma função para atualizar a pg para atualizar a foto nova

--- src/modules/auth/views/Perfil.vue
+++ src/modules/auth/views/Perfil.vue
@@ -2,9 +2,12 @@
 import { onMounted } from "vue";
 import { getAuth, signOut } from "firebase/auth";
 import { useRouter } from "vue-router";
-import { petStore } from "../store";
+import { ref } from "vue";
+import { petStore, imgStore } from "../store";
 
 const router = useRouter();
+
+const dialog = ref(false);
 
 onMounted(async () => {
   await petStore.pegarUser();
@@ -17,6 +20,10 @@ function handleSignOut() {
     router.push("/login");
   });
 }
+
+const reloadPage = () => {
+  location.reload();
+};
 </script>
 
 <template>
@@ -27,35 +34,74 @@ function handleSignOut() {
     <v-app-bar-title> Perfil do pet</v-app-bar-title>
   </v-app-bar>
 
-  <v-img width="100%" height="50%"></v-img>
-
-  <v-card
-    color="#053026"
-    width="90%"
-    elevation="10"
-    rounded="xl"
-    class="d-flex justify-space-between align-center py-4 mx-auto"
+  <v-img
+    min-width="50%"
+    max-width="100%"
+    aspect-ratio="1/1"
+    cover
+    :src="petStore.petDados?.profilePicture"
   >
-    <div class="w-50 ml-5">
-      <v-card-title class="text-white text-h4">{{
-        petStore.petDados.petName
-      }}</v-card-title>
-      <v-card-subtitle class="text-white py-4">
-        <v-icon color="white" icon="mdi-map-marker-outline pb-1"></v-icon>
-        {{ petStore.petDados?.address }}
-      </v-card-subtitle>
-    </div>
+    <v-btn
+      icon="mdi-pencil"
+      size="small"
+      alt="Editar foto"
+      @click="dialog = true"
+      location="top end"
+      position="absolute"
+      class="ma-3"
+    >
+    </v-btn>
+    <v-dialog v-model="dialog" width="100%">
+      <v-card>
+        <v-card-text class="w-100">
+          <v-file-input
+            label="Nova foto do pet mais lindo do mundo"
+            variant="underlined"
+            prepend-icon="mdi-camera"
+            ref="file"
+            id="image"
+            @change="imgStore.img()"
+            type="file"
+            clear-icon="w-100"
+          ></v-file-input>
+        </v-card-text>
+        <v-card-actions class="d-flex justify-space-around">
+          <v-btn @click="dialog = false">Cancelar</v-btn>
+          <v-btn @click="reloadPage()">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-card
+      position="absolute"
+      location="bottom"
+      color="#053026"
+      width="90%"
+      elevation="10"
+      rounded="xl"
+      class="d-flex justify-space-between align-center py-3 mb-4 mx-auto"
+    >
+      <div>
+        <v-card-title class="text-white text-capitalize text-h4">{{
+          petStore.petDados.petName
+        }}</v-card-title>
+        <v-card-subtitle class="text-white text-capitalize py-2">
+          <v-icon color="white" icon="mdi-map-marker-outline pb-1"></v-icon>
+          {{ petStore.petDados?.address }}
+        </v-card-subtitle>
+      </div>
 
-    <div class="w-50 text-right mr-5">
-      <v-icon color="#8dd8c1" icon="mdi-pencil ma-2"></v-icon>
-      <p class="text-white pt-4">Fêmea</p>
-    </div>
-  </v-card>
-
-  <v-card elevation="10" class="mt-4">
+      <div class="w-50 text-right mr-4 pb-1">
+        <v-icon color="#8dd8c1" icon="mdi-pencil ma-2"></v-icon>
+        <p class="text-grey-lighten-1 text-capitalize font-weight-light pt-4">
+          {{ petStore.petDados?.sexo }}
+        </p>
+      </div>
+    </v-card>
+  </v-img>
+  <v-card elevation="10" class="">
     <v-card-title>Restrições</v-card-title>
     <v-card-text>
-      <p class="py-2" v-for="restricao in petStore.petDados.restricao">
+      <p class="py-1 text-capitalize text-center" v-for="restricao in petStore.petDados.restricao">
         {{ restricao }}
       </p>
     </v-card-text>
@@ -63,11 +109,12 @@ function handleSignOut() {
 
   <v-sheet class="mx-auto mt-5" width="80%" height="100px" color="transparent">
     <v-btn
+      size="x-large"
       @click="handleSignOut"
-      color="#fe8b05"
+      rounded="xl"
+      color="#053026"
       block
-      rounded
-      class="pa-7 elevation-10 text-white"
+      class="elevation-10 text-white"
     >
       Log Out
     </v-btn>
