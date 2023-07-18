8d9c46312c3e5c66b497b76a9a6f98203756c7ec
Igor Muller <igormullersantos@hotmail.com>
2023-07-18 14:05:53 -0300

    HOT-FIX/End-tag

--- src/modules/auth/views/PetName.vue
+++ src/modules/auth/views/PetName.vue
@@ -1,18 +1,15 @@
 <script setup>
 import { ref } from "vue";
-import {petStore} from "../store"
+import { petStore } from "../store";
 import { reactive } from "vue";
 
 const petName = ref("");
 const adress = ref("");
 
-
-const payload = reactive({ 
+const payload = reactive({
   petName: petName,
-    adress: adress,  
-})
-
-
+  adress: adress,
+});
 </script>
 
 <template>
@@ -28,14 +25,14 @@ const payload = reactive({
         qual o nome do <br />seu pet?
       </h1>
       <v-text-field
-      variant="outlined"
-      rounded="pill"
-      color="#2a6141"
-      v-model="petName"
-      label="Nome do pet"
-      clearable
-      hide-details="auto"
-      class="w-100 mt-4 text-green-darken-4"
+        variant="outlined"
+        rounded="pill"
+        color="#2a6141"
+        v-model="petName"
+        label="Nome do pet"
+        clearable
+        hide-details="auto"
+        class="w-100 mt-4 text-green-darken-4"
       >
       </v-text-field>
       <h3 class="text-teal-darken-4">E onde vocês moram?</h3>
@@ -53,25 +50,13 @@ const payload = reactive({
     </v-card>
     <v-btn
       size="large"
-      @click="this.$router.push('/pet-detail')"
+      @click="petStore.proximo(payload), this.$router.push('/pet-detail')"
       rounded="xl"
       color="#053026"
-      v-model="adress"
-      label="Endereço do pet"
-      clearable
-      hide-details="auto"
-      class="w-100 mt-4 text-green-darken-4"
+      location="bottom"
+      position="fixed"
+      class="mb-16 text-white w-50"
+      >Próximo</v-btn
     >
-    </v-text-field>
-  </v-card>
-  <v-btn
-    size="large"
-    @click=" (petStore.proximo(payload)), (this.$router.push('/pet-detail'))"
-    rounded="xl"
-    color="#053026"
-    location="bottom"
-    position="fixed"
-    class="mb-16 text-white w-50"
-    >Próximo</v-btn
-  >
+  </v-container>
 </template>
